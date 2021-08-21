83799e2f23e7f5e8fdba58b6e3dac1e0720b67d2
AshuKulu <71241200+AshuKulu@users.noreply.github.com>
2021-08-21 11:37:53 +0530

    Add files via upload

---
+++ Speech To Text App/MainActivity.java
@@ -0,0 +1,50 @@
+package com.remarkskillmta.speechtotext;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button convert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text=findViewById(R.id.text);
+        convert=findViewById(R.id.convert);
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech To Text");
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1){
+            if(resultCode==RESULT_OK && data!=null){
+                ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                text.setText(result.get(0));
+            }
+        }
+    }
+}
---
+++ Speech To Text App/ReadMe.txt
@@ -0,0 +1,15 @@
+Hello Visitor 😊
+Thank You for coming here :)
+
+This app converts your voice/speech to words. Default language is english.
+It will help you while taking note from a audio/video content.
+I konw app's UI is bad, I just focused on Java logic part,
+You can copy the xml code to your android studio and design your own beautiful UI.
+.xml code and .java code are given in this folder
+Create empty activity in your Android Studio and copy paste this code to build your App.
+.apk file is also given, You can test it and let me know how it works and how can I modify it.
+
+< https://drive.google.com/drive/folders/1C6_9iGwuqLBbxD_5T2KDucJmxYHqYyR6?usp=sharing >
+[ Drive link to download and test the app ]
+
+Thank You again! 🤗
